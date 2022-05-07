1d261db0c8d0ea9baf9f9f2ba8bbafd2914e9093
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-05-07 21:22:08 +0200

    Final touch

--- PudelkoConsoleTest/Program.cs
+++ PudelkoConsoleTest/Program.cs
@@ -1,11 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PudelkoConsoleTest;
 using PudelkoLib;
 
 Pudelko pudelko = new Pudelko(1, unit: Pudelko.UnitOfMeasure.meter);
 Pudelko pudelko1 = new Pudelko(120, 120, 210, Pudelko.UnitOfMeasure.milimeter);
 Pudelko pudelko2 = new Pudelko(10, 10, 10, Pudelko.UnitOfMeasure.milimeter);
-
-List<Pudelko> list = new List<Pudelko>();
+Pudelko pudelko3 = new();
+Console.WriteLine(pudelko3.ToString());
+Console.WriteLine(pudelko3.ToString("m"));
+Console.WriteLine(pudelko3.ToString("cm"));
+Console.WriteLine(pudelko3.ToString("mm"));
+Console.WriteLine("======================");
+Console.WriteLine("Objetosc: " + pudelko3.Objetosc);
+Console.WriteLine("Pole: " + pudelko3.Pole);
+Console.WriteLine("======================");
+Console.WriteLine(Pudelko.Parse(pudelko3.ToString()).ToString());
+Console.WriteLine("======================");
+Pudelko test = new Pudelko(1,2,3);
+Console.WriteLine(test.ToString());
+Console.WriteLine("Skompresowane: " + PudelkoEXtension.Kompresuj(test).ToString())
+List <Pudelko> list = new List<Pudelko>();
 list.Add(pudelko1);
 list.Add(pudelko);
 list.Add(pudelko2);
